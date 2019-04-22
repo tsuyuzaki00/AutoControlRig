216957f2ee39d411b7fa8861040784e31b2b6c2a
GPDPocket <mugen.tsuyuzaki@gmail.com>
2019-04-22 16:21:04 +0900

    blendcolorコネクトの骨組みが完成

--- connectAttrColor.py
+++ connectAttrColor.py
@@ -1,15 +1,29 @@
 import pymel.core as pm
 
-pm.select('jntPrx_set')
-jntPrxs = pm.ls(sl=True, dag=True)
+jointName = 'lowLeg'
+RL = 'L'
 
-pm.select('jntFK_set')
-jntFKs = pm.ls(sl=True, dag=True)
+pm.select(jointName + '_jntPrx_' + RL)
+jntPrxs = pm.ls(sl=True)
 
-pm.select('jntIK_set')
-jntIKs = pm.ls(sl=True, dag=True)
+pm.select(jointName + '_jntFK_' + RL)
+jntFKs = pm.ls(sl=True)
 
-for i in range(10):
-    pm.connectAttr( jntFKs[i]+'.'+'rotate', jntPrxs[i]+'.'+'color2',f=True )
-    pm.connectAttr( jntIKs[i]+'.'+'rotate', jntPrxs[i]+'.'+'color1',f=True )
-    pm.connectAttr( blendColors[i]+'.'+'output', jntPrxs[i]+'.'+'rotate',f=True )
+pm.select(jointName + '_jntIK_' + RL)
+jntIKs = pm.ls(sl=True)
+
+pm.select(jointName + '_rotateBlend_' + RL)
+rotateBlends = pm.ls(sl=True)
+
+pm.select(jointName + '_translateBlend_' + RL)
+translateBlends = pm.ls(sl=True)
+
+for i in range(len(jntPrxs)):
+    pm.connectAttr( jntFKs[i]+'.'+'rotate', rotateBlends[i]+'.'+'color2',f=True )
+    pm.connectAttr( jntIKs[i]+'.'+'rotate', rotateBlends[i]+'.'+'color1',f=True )
+    pm.connectAttr( rotateBlends[i]+'.'+'output', jntPrxs[i]+'.'+'rotate',f=True )
+
+for i in range(len(jntPrxs)):
+    pm.connectAttr( jntFKs[i]+'.'+'translate', translateBlends[i]+'.'+'color2',f=True )
+    pm.connectAttr( jntIKs[i]+'.'+'translate', translateBlends[i]+'.'+'color1',f=True )
+    pm.connectAttr( translateBlends[i]+'.'+'output', jntPrxs[i]+'.'+'translate',f=True )
