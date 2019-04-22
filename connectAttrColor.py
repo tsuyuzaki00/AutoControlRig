4f746c5bee91afd4bc9aab6027330b87f8f0405f
GPDPocket <mugen.tsuyuzaki@gmail.com>
2019-04-22 21:22:31 +0900

    配列で出来るか検討中

--- connectAttrColor.py
+++ connectAttrColor.py
@@ -1,7 +1,11 @@
 import pymel.core as pm
 
-jointName = 'lowLeg'
-RL = 'L'
+#legNames = ['upLeg','lowLeg','ankle','foot']
+#armNames = ['upArm','lowArm','hand','foot','thumb','thumb01','thumb02','thumb03','index','index01','index02','index03','middle','middle01','middle02','middle03','ring','ring01','ring02','ring03','little','little01','little02','little03']
+#RLs = ['L','LT','R','RT']
+
+jointName = 'upLeg'
+RL = 'R'
 
 pm.select(jointName + '_jntPrx_' + RL)
 jntPrxs = pm.ls(sl=True)
