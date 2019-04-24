1dd93334842c8538070e1774fbf31ff60be2478b
DESKTOP1_Tsuyuzaki <mugen.tsuyuzaki@gmail.com>
2019-04-25 06:40:42 +0900

    blendColorの接続する方法のスクリプトを追加

--- blendColorConnect.py
+++ blendColorConnect.py
@@ -1,10 +1,25 @@
 import pymel.core as pm
 
+pm.select('upArmTwist_jntPrx_R','lowArmTwist_jntPrx_R')
+jntPrxs = pm.ls(sl=True)
 
+pm.select('upArmTwist_jntIK_R','lowArmTwist_jntIK_R')
+jntIKs = pm.ls(sl=True)
 
-pm.connectAttr( +'.FKIK', +'translateBlend_L.blender')
-pm.connectAttr( +'.FKIK', +'translateBlend_R.blender')
+pm.select('upArmTwist_jntFK_R','lowArmTwist_jntFK_R')
+jntFKs = pm.ls(sl=True)
 
-pm.connectAttr('hand_jntPrxIK_L.translate', 'hand_blendColor_L.color1')
-pm.connectAttr('hand_jntPrxFK_L.translate', 'hand_blendColor_L.color2')
-pm.connectAttr('hand_blendColor_L.output', 'hand_jnt_L.translate')
+pm.select('upArmTwist_rotateBlend_R','lowArmTwist_rotateBlend_R')
+rollblend = pm.ls(sl=True)
+
+pm.select('upArmTwist_translateBlend_R','lowArmTwist_translateBlend_R')
+trsblend = pm.ls(sl=True)
+
+for i in range(len(jntIKs)):
+    pm.connectAttr( jntIKs[i]+'.'+'translate', trsblend[i]+'.'+'color1',f=True )
+    pm.connectAttr( jntFKs[i]+'.'+'translate', trsblend[i]+'.'+'color2',f=True )
+    pm.connectAttr( trsblend[i]+'.'+'output', jntPrxs[i]+'.'+'translate',f=True )
+    
+    pm.connectAttr( jntIKs[i]+'.'+'rotate', rollblend[i]+'.'+'color1',f=True )
+    pm.connectAttr( jntFKs[i]+'.'+'rotate', rollblend[i]+'.'+'color2',f=True )
+    pm.connectAttr( rollblend[i]+'.'+'output', jntPrxs[i]+'.'+'rotate',f=True )
