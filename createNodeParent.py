3cbe443b706f4865cb63e565ee0e1c04e56a0a25
DESKTOP1_Tsuyuzaki <mugen.tsuyuzaki@gmail.com>
2019-05-08 01:02:42 +0900

    translateとrotateの位置をジョイントの位置にするスクリプトを追加。 名前をジョイントと同じ名前に変更。

--- createNodeParent.py
+++ createNodeParent.py
@@ -6,5 +6,7 @@ select = pm.select( name +'_jnt')
 jnts = pm.ls(sl=True, dag=True)
 
 for i in range(len(jnts)):
-    node = pm.createNode( 'transform',n='_trs')
+    node = pm.createNode( 'transform',n=jnts[i])
     pm.parent( node , jnts[i] )
+    pm.move( 0, 0, 0 ,ls=True)
+    pm.rotate( 0, 0, 0 , os=True)
