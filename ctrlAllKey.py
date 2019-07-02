58d7d6af6d08c08257483e6462dab96076a843f9
DESKTOP1_Tsuyuzaki <mugen.tsuyuzaki@gmail.com>
2019-07-02 15:10:14 +0900

    アクセスできるようにdef main():を作成 キーを打ち終わったら選択をクリアするように設定

--- ctrlAllKey.py
+++ ctrlAllKey.py
@@ -1,8 +1,10 @@
 import pymel.core as pm
 
-selects = pm.ls(sl=True, dag=True)
+def main():
+    selects = pm.ls(sl=True, dag=True)
 
-for allKey in selects:
-    if allKey.nodeType() == 'transform':
-        if '_ctrl' in allKey.name():
-            pm.setKeyframe(allKey)
+    for allKey in selects:
+        if allKey.nodeType() == 'transform':
+            if '_ctrl' in allKey.name():
+                pm.setKeyframe(allKey)
+                pm.select(cl = True)
