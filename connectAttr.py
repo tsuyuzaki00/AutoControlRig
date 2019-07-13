4a187ca68fec561d3c1dcb1d7da671f89aa5172c
GPDPocket <mugen.tsuyuzaki@gmail.com>
2019-07-13 22:34:39 +0900

    名前を検索してjntとjntPrxを接続できるように変更

--- connectAttr.py
+++ connectAttr.py
@@ -7,7 +7,8 @@ jnts = pm.ls(sl=True, dag=True)
 
 pm.select(name + '_jntPrx')
 jntPrxs = pm.ls(sl=True, dag=True)
+jntPrx =[i for i in jntPrxs if '_jntPrx' in i.name()]
 
 for i in range(len(jnts)):
-    pm.connectAttr( jntPrxs[i]+'.'+'translate', jnts[i]+'.'+'translate',f=True )
-    pm.connectAttr( jntPrxs[i]+'.'+'rotate', jnts[i]+'.'+'rotate',f=True )
+    pm.connectAttr( jntPrx[i]+'.'+'translate', jnts[i]+'.'+'translate',f=True )
+    pm.connectAttr( jntPrx[i]+'.'+'rotate', jnts[i]+'.'+'rotate',f=True )
