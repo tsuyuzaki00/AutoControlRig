251648ed1243418834c1dc3e9c8d170ef6e588cd
DESKTOP1_Tsuyuzaki <mugen.tsuyuzaki@gmail.com>
2019-07-28 11:31:13 +0900

    右は青、左は緑に階層のcurveのみを選んでカラー変更だけできるようにした。

--- nurbsCurveColor.py
+++ nurbsCurveColor.py
@@ -1,12 +1,9 @@
 import pymel.core as pm
 
-pm.select('shoulder_ctrl_L')
 selects = pm.ls(sl=True, dag=True)
 
 for ctrl in selects:
     if ctrl.nodeType() == 'nurbsCurve':
         pm.setAttr( ctrl+'.overrideEnabled', 1)
-        pm.setAttr( ctrl+'.overrideColor', 6)
-        
-        if '_ctrlFK' in ctrl.name():
-            pm.connectAttr( 'humanRigKit_armFKIK_ctrl_L.FKIK', ctrl+'.visibility' )
+        pm.setAttr( ctrl+'.overrideColor', 6)#Left
+        pm.setAttr( ctrl+'.overrideColor', 14)#Right
