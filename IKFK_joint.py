925f446aac7b248b875d855f89bcb257af8d4bb3
DESKTOP1_Tsuyuzaki <mugen.tsuyuzaki@gmail.com>
2019-08-17 21:41:09 +0900

    tabの修正

--- IKFK_joint.py
+++ IKFK_joint.py
@@ -1,26 +1,28 @@
 import pymel.core as pm
 
 def main():
-        trsAttr = 'translate'
-        rotateAttr = 'rotate'
-        blendc_inAttr1 = 'color1'
-        blendc_inAttr2 = 'color2'
-        blendc_outAttr = 'output'
+    name = 'dangoro_leg'
+    LR = '_R'
+    trsAttr = 'translate'
+    rotateAttr = 'rotate'
+    blendc_inAttr1 = 'color1'
+    blendc_inAttr2 = 'color2'
+    blendc_outAttr = 'output'
 
-        pm.select('dangoro_leg_jntPrx_L')
-        Prx = pm.ls(sl=True, dag=True)
+    pm.select(name + '_jntPrx' + LR)
+    Prx = pm.ls(sl=True, dag=True)
 
-        jntPrxs =[i for i in Prx if '_jntPrx' in i.name()]
-        jntFKs = [jntPrx.replace('_jntPrx', '_jntFK') for jntPrx in jntPrxs]
-        jntIKs = [jntPrx.replace('_jntPrx', '_jntIK') for jntPrx in jntPrxs]
-        blendT = [jntPrx.replace('_jntPrx', '_translateBlend') for jntPrx in jntPrxs]
-        blendR = [jntPrx.replace('_jntPrx', '_rotateBlend') for jntPrx in jntPrxs]
+    jntPrxs =[i for i in Prx if '_jntPrx' in i.name()]
+    jntFKs = [jntPrx.replace('_jntPrx', '_jntFK') for jntPrx in jntPrxs]
+    jntIKs = [jntPrx.replace('_jntPrx', '_jntIK') for jntPrx in jntPrxs]
+    blendT = [jntPrx.replace('_jntPrx', '_translateBlend') for jntPrx in jntPrxs]
+    blendR = [jntPrx.replace('_jntPrx', '_rotateBlend') for jntPrx in jntPrxs]
 
-        for i in range(len(jntPrxs)):
-                pm.connectAttr( jntIKs[i]+'.'+trsAttr, blendT[i]+'.'+blendc_inAttr1)
-                pm.connectAttr( jntFKs[i]+'.'+trsAttr, blendT[i]+'.'+blendc_inAttr2)
-                pm.connectAttr( blendT[i]+'.'+blendc_outAttr, jntPrxs[i]+'.'+trsAttr )
-    
-                pm.connectAttr( jntIKs[i]+'.'+rotateAttr, blendR[i]+'.'+blendc_inAttr1)
-                pm.connectAttr( jntFKs[i]+'.'+rotateAttr, blendR[i]+'.'+blendc_inAttr2)
-                pm.connectAttr( blendR[i]+'.'+blendc_outAttr, jntPrxs[i]+'.'+rotateAttr)
+    for i in range(len(jntPrxs)):
+        pm.connectAttr( jntIKs[i]+'.'+trsAttr, blendT[i]+'.'+blendc_inAttr1)
+        pm.connectAttr( jntFKs[i]+'.'+trsAttr, blendT[i]+'.'+blendc_inAttr2)
+        pm.connectAttr( blendT[i]+'.'+blendc_outAttr, jntPrxs[i]+'.'+trsAttr )
+        
+        pm.connectAttr( jntIKs[i]+'.'+rotateAttr, blendR[i]+'.'+blendc_inAttr1)
+        pm.connectAttr( jntFKs[i]+'.'+rotateAttr, blendR[i]+'.'+blendc_inAttr2)
+        pm.connectAttr( blendR[i]+'.'+blendc_outAttr, jntPrxs[i]+'.'+rotateAttr)
