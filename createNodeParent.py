ad16fa6de877f19d54b9688d77322010095935fe
DESKTOP1_Tsuyuzaki <mugen.tsuyuzaki@gmail.com>
2019-08-17 21:37:51 +0900

    ノードタイプを_trsに変更できるようにした

--- createNodeParent.py
+++ createNodeParent.py
@@ -5,8 +5,9 @@ name = 'root'
 select = pm.select( name +'_jnt')
 jnts = pm.ls(sl=True, dag=True)
 
+jntPrxs = [jntPrx.replace("_jnt",'_trs') for jntPrx in jnts]
 for i in range(len(jnts)):
-    node = pm.createNode( 'transform',n=jnts[i])
+    node = pm.createNode( 'transform',n=jntPrxs[i])
     pm.parent( node , jnts[i] )
     pm.move( 0, 0, 0 ,ls=True)
     pm.rotate( 0, 0, 0 , os=True)
