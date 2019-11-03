bc89376f65886a30bebfa0ee979d5b3925902dbc
GPDPocket <mugen.tsuyuzaki@gmail.com>
2019-11-04 04:27:53 +0900

    2つ選択したものがテキストの枠に入るように編集

--- nameConnect.py
+++ nameConnect.py
@@ -1,18 +1,30 @@
 import pymel.core as pm
 
-with pm.window( title='my tools', width=300 ):
+p =  pm.selected()
+if len(p) >= 2:
+    sel1 = pm.selected()[0]
+    sel2 = pm.selected()[1]
+else:
+    sel1 = ''
+    sel2 = ''
+    
+with pm.window( title='Node Connect', width=300 ):
     with pm.columnLayout( adjustableColumn=True ):
+        
+        pm.text( label='radio button' )
+        rdoGrp = pm.radioButtonGrp( numberOfRadioButtons=2,label='sel/grp',labelArray2=['sel', 'grp'],sl=1)
+        pm.separator()
         
         pm.text(label='check box' )
         boxGrp = pm.checkBoxGrp( numberOfCheckBoxes=3, label='connect', labelArray3=['Translate','Rotate','Scale'] )
         pm.separator()
         
         pm.text(label='OutNodeName' )
-        connectOut = pm.textFieldGrp(label='connectOut', pht='please connectOut text...' )
+        connectOut = pm.textFieldGrp(label='connectOut', pht='please connectOut text...',text=sel1)
         pm.separator()
         
         pm.text(label='InNodeName' )
-        connectIn = pm.textFieldGrp(label='connectIn', pht='please connectIn text...' )
+        connectIn = pm.textFieldGrp(label='connectIn', pht='please connectIn text...',text=sel2)
         pm.separator()
         
         pm.button( label='print' , c='main()' )
@@ -29,3 +41,5 @@ def main():
         pm.connectAttr( outText+'.'+'rotate', inText+'.'+'rotate',f=True )
     if boxScale == True:
         pm.connectAttr( outText+'.'+'scale', inText+'.'+'scale',f=True )
+        
+        
