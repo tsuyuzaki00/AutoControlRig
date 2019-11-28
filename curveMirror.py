8078ff1971cde2f012595e404a0065e51f21a211
tsuyuzaki <mugen.tsuyuzaki@gmail.com>
2019-11-28 18:26:20 +0900

    頂点を選択しLとRがある場合-Xに反転するスクリプトを作成

---
+++ curveMirror.py
@@ -0,0 +1,17 @@
+import pymel.core as pm
+
+def shapeMove():
+    sels = pm.ls(sl = True)
+    for j in range(len(sels)):
+        objName = sels[j].rsplit("[",1)
+        numText = objName[1].split(':')[-1].split(']')[0]
+        number = int(numText) + 1
+        for i in range(number):
+            posName = objName[0]+"["+ str(i) +"]"
+            oriPoss = pm.pointPosition( posName , w = True )
+            renamePos = posName.replace('L_', 'R_')
+        
+            pm.select(renamePos)
+            pm.move(oriPoss[0]*-1,oriPoss[1],oriPoss[2])
+            
+shapeMove()
