e32504040d79369c187c17f3c48baa5ef9945940
DESKTOP1_Tsuyuzaki <mugen.tsuyuzaki@gmail.com>
2020-05-15 20:46:53 +0900

    カーブを変更するスクリプトを作成。 ミラーやコントローラーカラー変更のスクリプトを修正

--- ctrlColorChenge.py
+++ ctrlColorChenge.py
@@ -1,8 +1,24 @@
 import pymel.core as pm
 
 sel = pm.selected()
-shapes = pm.listRelatives(sel[0:])
+shapes = pm.listRelatives(sel[0:], type='nurbsCurve')
 
 for shape in shapes:
     pm.setAttr(shape+'.overrideEnabled',1)
-    pm.setAttr(shape+'.overrideColor',18)
+    if shape.endswith('_CShape') == True:
+        #真ん中
+        pm.setAttr(shape+'.overrideColor',17) #ベースカラー、黄色
+        pm.setAttr(shape+'.overrideColor',14) #ベースカラー、グリーン
+        pm.setAttr(shape+'.overrideColor',27) #セカンドカラー、あっさり緑
+        pm.setAttr(shape+'.overrideColor',7 ) #オートサポートカラー、濃い緑
+    elif shape.endswith('_LShape') == True:
+        #左
+        pm.setAttr(shape+'.overrideColor',6 ) #ベースカラー、ブルー
+        pm.setAttr(shape+'.overrideColor',18) #セカンドカラー、シアン
+        pm.setAttr(shape+'.overrideColor',30) #オートサポートカラー、紫 
+    elif shape.endswith('_RShape') == True:
+        #右
+        pm.setAttr(shape+'.overrideColor',13) #ベースカラー、レッド
+        pm.setAttr(shape+'.overrideColor',20) #セカンドカラー、ピンク
+        pm.setAttr(shape+'.overrideColor',31) #オートサポートカラー、マゼンダ
+    
--- curveMirror.py
+++ curveMirror.py
@@ -9,7 +9,7 @@ def shapeMove():
         for i in range(number):
             posName = objName[0]+"["+ str(i) +"]"
             oriPoss = pm.pointPosition( posName , w = True )
-            renamePos = posName.replace('L_', 'R_')
+            renamePos = posName.replace('_L', '_R')
         
             pm.select(renamePos)
             pm.move(oriPoss[0]*-1,oriPoss[1],oriPoss[2])
