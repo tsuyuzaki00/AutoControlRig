99421b8aff6827c2ccfff0cae18b1eae9d469e3e
DESKTOP1_Tsuyuzaki <mugen.tsuyuzaki@gmail.com>
2020-06-14 01:48:40 +0900

    エラーの解決

--- autoRename.py
+++ autoRename.py
@@ -13,74 +13,93 @@ def scene(sel):
 
 def obj(sel):
     part = sel.split("_")
-    obj = part[0]
-    return obj
+    if part[0] == node(sel):
+        obj = part[1]
+        return obj
+    else :
+        obj = part[0]
+        return obj
 
 def node(sel):
-    sels = pm.selected()
-    for sel in sels:
-        if sel.nodeType() == 'transform':
-            if pm.listRelatives(sel, c = True, type = 'mesh'):
-                node = 'geo'
-            elif pm.listRelatives(sel, c = True, type = 'nurbsCurve'):
-                node = 'ctrl'
-            elif pm.listRelatives(sel, c = True, type = 'camera'):
-                node = 'cam'
-            elif pm.listRelatives(sel, c = True, type = 'locator'):
-                node = 'loc'
-            elif pm.listRelatives(sel, c = True, type = 'imagePlane'):
-                node = 'image'
-            elif pm.listRelatives(sel, c = True, type = 'spotLight'):
-                node = 'stl'
-            elif pm.listRelatives(sel, c = True, type = 'ambientLight'):
-                node = 'atl'
-            elif pm.listRelatives(sel, c = True, type = 'pointLight'):
-                node = 'ptl'
-            elif pm.listRelatives(sel, c = True, type = 'directionalLight'):
-                node = 'dtl'
-            #transNode
-            'null'
-            'offset'
-            'move' 
-            'space'
-            'trs'
-            'grp'
-        elif sel.nodeType() == 'joint':
-            node = 'jnt' #joint
-        elif sel.nodeType() == 'parentConstraint':
-            node = 'prc' #ParentConstraint
-        elif sel.nodeType() == 'pointConstraint':
-            node = 'ptc' #PointConstraint
-        elif sel.nodeType() == 'orientConstraint':
-            node = 'otc' #OrientConstraint
-        elif sel.nodeType() == 'scaleConstraint':
-            node = 'slc' #ScaleConstraint
-        elif sel.nodeType() == 'aimConstraint':
-            node = 'amc' #AimConstraint
-        elif sel.nodeType() == 'poleVectorConstraint':
-            node = 'pvc' #PoleVectorConstraint
-        elif sel.nodeType() == 'IKhandle':
-            node = 'ikh' #IKhandle
-        elif sel.nodeType() == 'effector':
-            node = 'eft' #effector
-        #elif sel.nodeType() == 'parentConstraint':
-            node = 'mat' #material
-            node = 'color' #baseColor
-            node = 'nmp' #normalMap
-            node = 'cnd' #condition
-            node = 'mdp' #multiplyDivide Power
-            node = 'pma' #PlusMinusAverage
-            node = 'rvs' #Reverse
-            node = 'clp' #Clamp
-            node = 'bdc' #Blend Colors
-            node = 'mmx' #multMatrix
-            node = 'dmx' #decomposeMatrix
-            node = 'cmx' #composeMatrix
-    return node
+    if sel.nodeType() == 'transform':
+        if pm.listRelatives(sel, c = True, type = 'mesh'):
+            node = 'geo'
+            return node
+        elif pm.listRelatives(sel, c = True, type = 'nurbsCurve'):
+            node = 'ctrl'
+            return node
+        elif pm.listRelatives(sel, c = True, type = 'camera'):
+            node = 'cam'
+            return node
+        elif pm.listRelatives(sel, c = True, type = 'locator'):
+            node = 'loc'
+            return node
+        elif pm.listRelatives(sel, c = True, type = 'imagePlane'):
+            node = 'image'
+            return node
+        elif pm.listRelatives(sel, c = True, type = 'spotLight'):
+            node = 'stl'
+            return node
+        elif pm.listRelatives(sel, c = True, type = 'ambientLight'):
+            node = 'atl'
+            return node
+        elif pm.listRelatives(sel, c = True, type = 'pointLight'):
+            node = 'ptl'
+            return node
+        elif pm.listRelatives(sel, c = True, type = 'directionalLight'):
+            node = 'dtl'
+            return node
+        #transNode
+        'null'
+        'offset'
+        'move' 
+        'space'
+        'trs'
+        'grp'
+    elif sel.nodeType() == 'joint':
+        node = 'jnt' #joint
+        return node
+    elif sel.nodeType() == 'parentConstraint':
+        node = 'prc' #ParentConstraint
+        return node
+    elif sel.nodeType() == 'pointConstraint':
+        node = 'ptc' #PointConstraint
+        return node
+    elif sel.nodeType() == 'orientConstraint':
+        node = 'otc' #OrientConstraint
+        return node
+    elif sel.nodeType() == 'scaleConstraint':
+        node = 'slc' #ScaleConstraint
+        return node
+    elif sel.nodeType() == 'aimConstraint':
+        node = 'amc' #AimConstraint
+        return node
+    elif sel.nodeType() == 'poleVectorConstraint':
+        node = 'pvc' #PoleVectorConstraint
+        return node
+    elif sel.nodeType() == 'IKhandle':
+        node = 'ikh' #IKhandle
+        return node
+    elif sel.nodeType() == 'effector':
+        node = 'eft' #effector
+        return node
+    #elif sel.nodeType() == 'parentConstraint':
+        node = 'mat' #material
+        node = 'color' #baseColor
+        node = 'nmp' #normalMap
+        node = 'cnd' #condition
+        node = 'mdp' #multiplyDivide Power
+        node = 'pma' #PlusMinusAverage
+        node = 'rvs' #Reverse
+        node = 'clp' #Clamp
+        node = 'bdc' #Blend Colors
+        node = 'mmx' #multMatrix
+        node = 'dmx' #decomposeMatrix
+        node = 'cmx' #composeMatrix
 
 def other(sel):
     other = 'C'
-    if node(node) == 'joint':
+    if 'joint' == node(sel):
         if pm.listRelatives(sel, c = True) == []:
             other = 'CT'
     'L'
@@ -88,7 +107,7 @@ def other(sel):
     'R'
     'RT'
 
-    if node(node) == 'geo':
+    if 'geo' == node(sel):
         other = '1'.zfill(3)
     return other
 
@@ -99,5 +118,3 @@ def main():
         names = [l for l in lists if l != '']
         autoRename = '_'.join(names)
         pm.rename(sel, autoRename)
-
-main()
