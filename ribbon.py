6f062f634aa300520c0836e17bb29754b0ace61c
tsuyuzakitatsuya <tsuyuzaki.tatsuya@engi-st.jp>
2020-10-19 19:03:32 +0900

    ribbonのクラス化

--- ribbon.py
+++ ribbon.py
@@ -1,26 +1,36 @@
 import pymel.core as pm
 import maya.mel as mel
 
-#public
-width = 16
-Ucount = 8
-length = 1.5
+class RibbonClass:
+    def __init__(self):
+        self.sels = pm.selected()
+        self.pos = 'C'
+        self.scene = 'scene'
+        self.obj = 'ribbon'
+        self.num = 00
 
-#private
-melUcount = Ucount + 1
-nurbsName = 'C_ribbon_nurb_scene_00'
-hairName = 'C_hairSystem_null_scene_00'
+    def settingRibbon(self, width = 16, Ucount = 4, length = 1.5):
+        melUcount = Ucount + 1
+        pm.nurbsPlane( ax = [0, 1, 0], d = 1, w = width, u = Ucount, v = 1, lr = length * 0.1, ch = False, n = '_'.join( [self.pos, self.obj, 'nurb', self.scene, '00']))
+        mel.eval('string $Ucount = "%s";' % melUcount)
+        #createHair(name Ucount,Vcount,PointPerHair,CreateRestCurves,PassiveFill,EdgeBounded,Equalize,Length,Randomization,Output,Dynamic||Static,PlaceHairsInto)
+        mel.eval('createHair $Ucount 1 10 0 0 1 0 5 0 2 1 1')
+        hairParents = pm.rename('hairSystem1Follicles', '_'.join( [self.pos, self.obj, 'grp', self.scene, '00']))
+        pm.delete('nucleus1', 'hairSystem1OutputCurves','hairSystem1')
+        parentFollicle = pm.listRelatives(hairParents, c = True)
+        for i in range(len(parentFollicle)):
+            deleteCurve = pm.listRelatives(parentFollicle[i] , c = True, typ = 'transform')
+            pm.delete(deleteCurve[0])
+            pm.select(parentFollicle[i])
+            pm.rename(parentFollicle[i], '_'.join( [self.pos, self.obj, 'foll', self.scene, str(i).zfill(2)]))
+            pm.joint(r = True, rad= 0.1, n = '_'.join( [self.pos, self.obj, 'jntRib', self.scene, str(i).zfill(2)]))
 
-pm.nurbsPlane( ax = [0, 1, 0], d = 1, w = width, u = Ucount, v = 1, lr = length * 0.1, ch = False, n = nurbsName)
-mel.eval('string $Ucount = "%s";' % melUcount)
-#createHair(name Ucount,Vcount,PointPerHair,CreateRestCurves,PassiveFill,EdgeBounded,Equalize,Length,Randomization,Output,Dynamic||Static,PlaceHairsInto)
-mel.eval('createHair $Ucount 1 10 0 0 1 0 5 0 2 1 1')
-hairParents = pm.rename('hairSystem1Follicles', hairName)
-pm.delete('nucleus1', 'hairSystem1OutputCurves','hairSystem1')
-parentFollicle = pm.listRelatives(hairParents, c = True)
-for follicle in parentFollicle:
-    deleteCurve = pm.listRelatives(follicle , c = True, typ = 'transform')
-    pm.delete(deleteCurve[0])
-    pm.select(follicle)
-    pm.rename(follicle, 'C_hair_foll_scene_00')
-    pm.joint(r = True, rad= 0.1, n = 'C_hair_jnt_scene_00')
+    def jointSelectRibbon(self):
+        pass
+
+    def nurbsCVSelectRibbon(self):
+        for sel in self.sels:
+            print sel[-1][-1]
+
+ribbonClass = RibbonClass()
+ribbonClass.nurbsCVSelectRibbon()
