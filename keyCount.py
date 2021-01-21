7adccfe02159a77a17668d0d6411f97d4dfe8d54
tsuyuzakitatsuya <tsuyuzaki.tatsuya@engi-st.jp>
2021-01-21 19:07:04 +0900

    何個keyを打つかに変更

--- keyCount.py
+++ keyCount.py
@@ -1,9 +1,9 @@
 import pymel.core as pm
 
-def countKey(interval = 10, threeKeys = 1, sels = ''):
+def countKey(interval = 10, keyNum = 4, grpKey = 1, sels = ''):
     for sel in sels:
         pm.select(sel)
-        num = 1 + 2 * threeKeys
+        num = keyNum * grpKey
         nowKey = pm.currentTime( query=True )
         for loopNum in range(num):
             pm.currentTime( nowKey )
@@ -12,5 +12,5 @@ def countKey(interval = 10, threeKeys = 1, sels = ''):
 
 def main():
     sels = pm.selected()
-    countKey(interval = 10, threeKeys = 1, sels = sels)
+    countKey(interval = 5, keyNum = 4, grpKey = 1, sels = sels)
 
