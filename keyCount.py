e7f0dbb86378a59d21a84609cfff594871b8c84b
tsuyuzakitatsuya <tsuyuzaki.tatsuya@engi-st.jp>
2021-01-25 19:05:54 +0900

    複数選択で同じ場所にkeyを打つ関数を作成

--- keyCount.py
+++ keyCount.py
@@ -1,5 +1,14 @@
 import pymel.core as pm
 
+def multiCountKey(interval = 10, keyNum = 4, grpKey = 1, sels = ''):
+    pm.select(sels)
+    num = keyNum * grpKey
+    nowKey = pm.currentTime( query=True )
+    for loopNum in range(num):
+        pm.currentTime( nowKey )
+        pm.setKeyframe()
+        nowKey += interval
+
 def countKey(interval = 10, keyNum = 4, grpKey = 1, sels = ''):
     for sel in sels:
         pm.select(sel)
@@ -12,5 +21,5 @@ def countKey(interval = 10, keyNum = 4, grpKey = 1, sels = ''):
 
 def main():
     sels = pm.selected()
-    countKey(interval = 5, keyNum = 4, grpKey = 1, sels = sels)
-
+    multiCountKey(interval = 5, keyNum = 4, grpKey = 1, sels = sels)
+    #countKey(interval = 5, keyNum = 4, grpKey = 1, sels = sels)
